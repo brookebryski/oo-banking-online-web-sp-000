ec81f52d33cd7b7b91a9dc1d5804ec6234fa89d7
chic-state-0895 <brookeabryski@gmail.com>
2020-09-20 00:39:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && @status == "pending" && @sender.balance > @amount
+    if @sender.valid? && @receiver.valid? && @status == "pending" && @sender.balance > @amount
     @sender.balance -= @amount
     @receiver.balance +=@amount
     @status = "complete"
