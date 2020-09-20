fadd7ac2248f17fc4f287732fc53995898e5c716
chic-state-0895 <brookeabryski@gmail.com>
2020-09-20 00:41:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
     @sender.balance -= @amount
     @receiver.balance +=@amount
     @status = "complete"
-  elsif @sender.balance < @amount || @sender.status != "open"
+  else @sender.balance < @amount || @sender.status != "open"
     @status = "rejected"
     puts "Transaction rejected. Please check your account balance."
   end
